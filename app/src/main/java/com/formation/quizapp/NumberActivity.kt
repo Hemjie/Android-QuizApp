d971920a7afd58448002342706dc1ea9a7f0049d
Hemjie <marjolaine.vilain@gmail.com>
2021-04-15 14:20:12 +0200

    connexion du bouton 5 vers la page question

--- app/src/main/java/com/formation/quizapp/NumberActivity.kt
+++ app/src/main/java/com/formation/quizapp/NumberActivity.kt
@@ -1,17 +1,38 @@
 package com.formation.quizapp
 
-import android.content.SharedPreferences
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class NumberActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number)
 
-        val action = intent.action
+
+        val btn5 = findViewById<Button>(R.id.nb_btn_5)
+        Log.i("NumberActivity", "${btn5.text}")
+
+        btn5.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            intent.action = Intent.ACTION_VIEW
+        }
+
+        val btn10: Button = findViewById(R.id.nb_btn_10)
+        val btn15: Button = findViewById(R.id.nb_btn_15)
+        val btn20: Button = findViewById(R.id.nb_btn_20)
     }
+
+    /*override fun onClick(v: View) {
+            val btn5 = findViewById<Button>(R.id.nb_btn_5)
+            Log.i("NumberActivity", "${btn5.text}")
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+
+    }*/
+
 }

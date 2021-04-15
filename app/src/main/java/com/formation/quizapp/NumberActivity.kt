f426d1f2fffb48b239012ac510a87e85caa5e94a
Hemjie <marjolaine.vilain@gmail.com>
2021-04-15 10:10:22 +0200

    création de l'activité nombre de questions

---
+++ app/src/main/java/com/formation/quizapp/NumberActivity.kt
@@ -0,0 +1,11 @@
+package com.formation.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number)
+    }
+}
